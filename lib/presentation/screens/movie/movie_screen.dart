d083867c2e3dae4bbf8e270674f032de339c7889
DidierDev672 <ariasdev672@gmail.com>
2024-05-05 11:34:20 -0500

    Fin de la sección de populares

--- lib/presentation/screens/movie/movie_screen.dart
+++ lib/presentation/screens/movie/movie_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:movie_app/config/helpers/human_formats.dart';
 import 'package:movie_app/presentation/providers/providers.dart';
 import 'package:movie_app/widget/movies/movie_rating.dart';
 import 'package:movie_app/widget/movies/similar_movies.dart';
